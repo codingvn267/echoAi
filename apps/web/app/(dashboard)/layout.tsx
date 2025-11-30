55ddd2a475ae5feaed74b274802da4375b22aebc
tungnguyen267 <tungthanhnguyen267@gmail.com>
2025-11-30 14:09:19 -0600

    07: dashboard-layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,11 +1,8 @@
-import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
-import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guards";
+import { DashboardLayout } from "@/modules/dashboard/ui/layouts/dashboard_layout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <AuthGuard>
-      <OrganizationGuard>{children}</OrganizationGuard>
-    </AuthGuard>
+    <DashboardLayout> {children}</DashboardLayout>
   );
 };
 
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -25,8 +25,7 @@ export default function RootLayout({
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
-        <ClerkProvider appearance={{
-        theme: dark}}>
+        <ClerkProvider>
           <Providers>{children}</Providers>
         </ClerkProvider>
       </body>
