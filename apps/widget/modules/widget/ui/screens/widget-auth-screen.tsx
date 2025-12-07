28b2f184a5add41d4fbe02ff354af6d737eb2fde
tungnguyen267 <tungthanhnguyen267@gmail.com>
2025-12-06 22:39:22 -0600

    12: widget loading

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -14,7 +14,9 @@ import { z } from "zod";
 import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { userAgent } from "next/server";
-import { Doc } from "@workspace/backend/_generated/dataModel";
+import { Doc, Id } from "@workspace/backend/_generated/dataModel";
+import { contactSessionIdAtomFamily, organizationIdAtom } from "../../atoms/widget_atoms";
+import { useAtomValue, useSetAtom } from "jotai";
 
 const formSchema = z.object({
   name: z.string().min(1, "Name is required"),
@@ -25,6 +27,9 @@ const formSchema = z.object({
 const organizationId = "123";
 
 export const WidgetAuthScreen = () => {
+  const organizationId = useAtomValue(organizationIdAtom);
+  const setContactSessionId = useSetAtom(contactSessionIdAtomFamily(organizationId || "")
+  )
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -59,7 +64,8 @@ export const WidgetAuthScreen = () => {
       organizationId,
       metadata,
     });
-    console.log(contactSessionId);
+    
+    setContactSessionId(contactSessionId as Id<"contactSessions">);
   };
   return (
     <>
