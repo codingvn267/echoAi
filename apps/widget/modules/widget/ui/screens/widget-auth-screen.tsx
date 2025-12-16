6551d13823077a075a4ebb5cdfb098cc5b395ce3
tungnguyen267 <tungthanhnguyen267@gmail.com>
2025-12-15 22:38:10 -0600

    15: Infinite scroll

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -15,7 +15,7 @@ import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { userAgent } from "next/server";
 import { Doc, Id } from "@workspace/backend/_generated/dataModel";
-import { contactSessionIdAtomFamily, organizationIdAtom } from "../../atoms/widget_atoms";
+import { contactSessionIdAtomFamily, organizationIdAtom, screenAtom } from "../../atoms/widget_atoms";
 import { useAtomValue, useSetAtom } from "jotai";
 
 const formSchema = z.object({
@@ -23,10 +23,8 @@ const formSchema = z.object({
   email: z.string().email("Invalid email address"),
 });
 
-// Temporary test organizationId, before we add state management
-// const organizationId = "123";
-
 export const WidgetAuthScreen = () => {
+  const setScreen = useSetAtom(screenAtom);
   const organizationId = useAtomValue(organizationIdAtom);
   const setContactSessionId = useSetAtom(contactSessionIdAtomFamily(organizationId || "")
   )
@@ -66,6 +64,7 @@ export const WidgetAuthScreen = () => {
     });
     
     setContactSessionId(contactSessionId as Id<"contactSessions">);
+    setScreen("selection");
   };
   return (
     <>
