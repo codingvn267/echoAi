784f9b49fcb9d7953ac22adbbd5ff141a65bde3d
tungnguyen267 <tungthanhnguyen267@gmail.com>
2026-01-06 18:47:57 -0600

    20-Generating Embedding

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -3,7 +3,7 @@ import { Agent } from "@convex-dev/agent";
 import { components } from "@workspace/backend/_generated/api.js";
 
 export const supportAgent = new Agent(components.agent, {
-  chat: openai.chat("gpt-4.1-mini"),
+  chat: openai.chat("gpt-4o-mini"),
   instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation.
                 use "escalateConversation" tool when user expresses frustration, or requests a human explicitly`,
 });
