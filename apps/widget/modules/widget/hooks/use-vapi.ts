8e1fca6788b138f296f90b9c2e122d1713bcd05f
tungnguyen267 <tungthanhnguyen267@gmail.com>
2026-03-03 22:08:09 -0600

    28: widget vapi

--- apps/widget/modules/widget/hooks/use-vapi.ts
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -1,5 +1,7 @@
 import Vapi from "@vapi-ai/web";
 import { useEffect, useState } from "react";
+import { vapiSecretsAtom, widgetSettingsAtom } from "../atoms/widget_atoms";
+import { useAtomValue } from "jotai";
 
 interface TranscriptMessage {
   role: "user" | "assistant";
@@ -7,6 +9,8 @@ interface TranscriptMessage {
 }
 
 export const useVapi = () => {
+  const vapiSecrets = useAtomValue(vapiSecretsAtom);
+  const widgetSettings = useAtomValue(widgetSettingsAtom);
   const [vapi, setVapi] = useState<Vapi | null>(null);
   const [isConnected, setIsConnected] = useState(false);
   const [isConnecting, setIsConnecting] = useState(false);
@@ -15,8 +19,12 @@ export const useVapi = () => {
 
 
   useEffect(() => {
+
+    if (!vapiSecrets) {
+      return;
+    }
     //Only for testing the Vapi API, otherwise customers will provide their own API keys.
-    const vapiInstance = new Vapi("");
+    const vapiInstance = new Vapi(vapiSecrets.publicApiKey);
     setVapi(vapiInstance);
 
     vapiInstance.on("call-start", () => {
@@ -62,11 +70,13 @@ export const useVapi = () => {
   }, []);
 
   const startCall = () => {
+    if (!vapiSecrets || !widgetSettings?.vapiSettings?.assistantId) {
+      return;
+    }
     setIsConnecting(true);
 
     if (vapi) {
-    // Only for testing the Vapi API, otherwise customers will provide their own API keys
-      vapi.start("")
+      vapi.start(widgetSettings.vapiSettings.assistantId);
     }
   }
 
